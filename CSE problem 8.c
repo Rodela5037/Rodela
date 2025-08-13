b0cd9bc2a44919e09ffe56e753927f0df9ad12dd
Rodela5037 <rezowanahossain288@gmail.com>
2025-08-13 21:11:52 +0600

    Update CSE problem 8.c

--- CSE problem 8.c
+++ CSE problem 8.c
@@ -1,24 +1,16 @@
-Write a C program to check whether a given year is a leap year or not using if-else and nested if-else statements. The user will input a year, and the program should display whether it is a leap year.
-  #include <stdio.h>
-
-int main() {
+Write a C program to check whether a given year is a leap year or not using if-else statements. The user will input a year, and the program should display whether it is a leap year.
+#include<stdio.h>
+int main()
+{
     int year;
-    printf("Enter year:\n");
-    scanf("%d", &year);
-
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                printf("%d is a leap year\n", year);
-            } else {
-                printf("%d is not a leap year\n", year);
-            }
-        } else {
-            printf("%d is a leap year\n", year);
-        }
-    } else {
-        printf("%d is not a leap year\n", year);
-    }
-
-    return 0;
+    printf("enter year:\n");
+    scanf("%d",&year);
+    if( year%4==0 )
+    {
+        printf(" leap year is %d",year);
+    } else if( year%100==0 && year%400==0 )
+    {
+        printf("leap year is %d",year);
+    } else{ printf(" not leap year");}
 }
+
